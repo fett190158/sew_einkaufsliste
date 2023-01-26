d6ff9f5ee8720db811ecacc3d62ef5146977457f
fett190158 <leinad.13.3@gmx.at>
2023-01-26 17:30:53 +0100

    reworked datamodel

--- Shopping List/datamodel.py
+++ Shopping List/datamodel.py
@@ -1,17 +1,16 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey    
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
 
 
-class ShoppingList(Base):
-    __tablename__ = "Shopping_List"
-
-    ID = Column(Integer, primary_key=True)
-    Name = Column(String)
-    Date = Column(Date)
-    Product = relationship("Product")
+ShoppingList_Product = Table(
+    'ShoppingList_Product', Base.metadata,
+    Column("Shopping_Lists_ID", Integer, ForeignKey("Shopping_List.ID"),
+           primary_key=True),
+    Column("Products_ID", Integer, ForeignKey("Product.ID"),
+           primary_key=True))
 
 
 class Product(Base):
@@ -22,4 +21,16 @@ class Product(Base):
     Quantity = Column(Integer)
     Volume = Column(Integer, nullable=True)
     Weight = Column(Integer, nullable=True)
-    ShoppingList_ID = Column(Integer, ForeignKey("Shopping_List.ID"))
+    Shopping_Lists = relationship("ShoppingList",
+                                  secondary=ShoppingList_Product,
+                                  back_populates="Product")
+
+
+class ShoppingList(Base):
+    __tablename__ = "Shopping_List"
+
+    ID = Column(Integer, primary_key=True)
+    Name = Column(String)
+    Date = Column(String)
+    Products = relationship("Product", secondary=ShoppingList_Product,
+                            back_populates="Shopping_Lists")
